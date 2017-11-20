a793f2311de65ee33b433a3995fba32cfa09a0c4
Astrid <astridronja.novicky@gmail.com>
2017-11-20 22:46:46 +0000

    initial commit

---
+++ django_simple_forum/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class DjangoSimpleForumConfig(AppConfig):
+    name = 'django_simple_forum'
